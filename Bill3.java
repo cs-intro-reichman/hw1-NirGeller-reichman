81e4e0a462240c185ca7ba1a282d8395ca4e6a72
NirGeller-reichman <nir.gellerchipchev@post.runi.ac.il>
2025-10-31 13:16:27 +0200

    solution

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,18 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name = args[0];
+	    String name1 = args[1];
+		String name2 =args [2];
+		String bill =args[3];
+		int totalbill= Integer.parseInt(bill);
+
+	    
+		double eachbill = totalbill / 3.0;
+	    eachbill=Math.ceil(eachbill);
+		System.out.println("Dear " +name2 +", "+ name1 + ", and " + name + ": pay " +eachbill +" Shekels each.");
+		
+
+
 	}
 }
