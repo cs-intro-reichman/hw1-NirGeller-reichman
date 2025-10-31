4fbad6fc8d83378973d1bd2321bc5fe7da964079
NirGeller-reichman <nir.gellerchipchev@post.runi.ac.il>
2025-10-31 13:42:56 +0200

    numwords solution

--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,15 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    String inputnum= args[0];
+		int thenumber = Integer.parseInt(inputnum);
+		int hundreds = thenumber/100;
+		
+		int secondstep = thenumber-(hundreds*100);
+		int tens= secondstep/10;
+		int units= secondstep-(tens*10);
+		System.out.println(hundreds +" hundreds, "+ tens +" tens, "+ "and " + units +" ones");
+		
+
 	}
 }
