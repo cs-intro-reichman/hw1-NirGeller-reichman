845e9869c5dae54d70ce7d7faed8909b07bab55f
NirGeller-reichman <nir.gellerchipchev@post.runi.ac.il>
2025-10-31 13:26:03 +0200

    favcalc solution

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,13 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		String currentvalue = args[0];
+		String interest = args[1];
+		String years = args[2];
+		double interstnum = Double.parseDouble(interest)/100.0;
+		double currentvaluenum = Double.parseDouble(currentvalue);
+		int yearsnum = Integer.parseInt(years);
+		double futurevalue =currentvaluenum* Math.pow((1.0 + interstnum),yearsnum);
+		System.out.println((int) futurevalue);
 	}
 }
