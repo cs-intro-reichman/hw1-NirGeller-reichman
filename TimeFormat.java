4c3354165d1883a71b9ce2c4f1f73268229bc71d
NirGeller-reichman <nir.gellerchipchev@post.runi.ac.il>
2025-11-01 18:12:19 +0200

    fixed timeformat new

--- TimeFormat.java
+++ TimeFormat.java
@@ -13,106 +13,8 @@ public class TimeFormat {
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         
-		if (hours == 13) 
-		{
-			if (minutes < 10) {
-				System.err.println("1" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("1" + ":" + minutes + " PM");	
-
-		}
-		else if (hours == 14) 
-		{
-			if (minutes < 10) {
-				System.err.println("2" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("2" + ":" + minutes + " PM");	
-
-		}
-		else if (hours == 15) 
-		{
-			if (minutes < 10) {
-				System.err.println("3" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("3" + ":" + minutes + " PM");	
-
-		}
-		else if (hours == 16) 
-		{
-			if (minutes < 10) {
-				System.err.println("4" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("4" + ":" + minutes + " PM");	
-
-		}
-		else if (hours == 17) 
-		{
-			if (minutes < 10) {
-				System.err.println("5" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("5" + ":" + minutes + " PM");	
-
-		}
-		else if (hours == 18) 
-		{
-			if (minutes < 10) {
-				System.err.println("6" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("6" + ":" + minutes + " PM");	
-
-		}
-		else if (hours == 19) 
-		{
-			if (minutes < 10) {
-				System.err.println("7" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("7" + ":" + minutes + " PM");	
-
-		}
-		else if (hours == 20) 
-		{
-			if (minutes < 10) {
-				System.err.println("8" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("8" + ":" + minutes + " PM");	
-
-		}
-		else if (hours == 21) 
-		{
-			if (minutes < 10) {
-				System.err.println("9" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("9" + ":" + minutes + " PM");	
-
-		}
-		else if (hours == 22) 
-		{
-			if (minutes < 10) {
-				System.err.println("10" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("10" + ":" + minutes + " PM");	
-
-		}
-		else if (hours == 23) 
-		{
-			if (minutes < 10) {
-				System.err.println("11" + ":" + "0" + minutes + " PM");
-			}
-			else
-		System.err.println("11" + ":" + minutes + " PM");	
-
-		}
-		else if (9<hours && hours<12) {
+		
+		if (9<hours && hours<12) {
 			if (minutes < 10) {
 				System.err.println(hours + ":" + "0" + minutes + " AM");
 			}
@@ -144,14 +46,17 @@ public class TimeFormat {
 				
 			System.err.println("0" + ":" + minutes + " AM");
 			}
-			
-
-			
 		}
-
-				
-	
-		if (hours == 24) {
+			else if (hours >12 && hours <24) {
+				hours = hours - 12;
+				if (minutes < 10) {
+					System.err.println(hours + ":" + "0" + minutes + " PM");
+				}
+				else 
+				System.err.println(hours + ":" + minutes + " PM");
+			}
+		
+		    else if (hours == 24) {
 			if (minutes < 10) {
 				System.err.println("0" + ":" + "0" + minutes + " AM");
 			}
