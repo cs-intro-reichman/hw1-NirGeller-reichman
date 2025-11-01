537e61e50badb970e82793dd67d75988668f668a
NirGeller-reichman <nir.gellerchipchev@post.runi.ac.il>
2025-11-01 17:22:40 +0200

    fixed calc

--- FVCalc.java
+++ FVCalc.java
@@ -8,6 +8,6 @@ public class FVCalc {
 		double currentvaluenum = Double.parseDouble(currentvalue);
 		int yearsnum = Integer.parseInt(years);
 		double futurevalue =currentvaluenum* Math.pow((1.0 + interstnum),yearsnum);
-		System.out.println("after " + years + " years, $" + currentvalue + " saved at " + interest + ".0%25 will yeald $" + (int) futurevalue);
+		System.out.println("after " + years + " years, $" + currentvalue + " saved at " + interstnum*100 + "% will yeald $" + (int) futurevalue);
 	}
 }
